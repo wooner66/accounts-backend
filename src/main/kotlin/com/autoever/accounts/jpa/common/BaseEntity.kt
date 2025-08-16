bb3db35c19762a6172dab227b45f96c62ecc3de9
wooner <>
2025-08-16 17:42:36 +0900

    feat: jpa BaseEntity 추가

---
+++ src/main/kotlin/com/autoever/accounts/jpa/common/BaseEntity.kt
@@ -0,0 +1,22 @@
+package com.autoever.accounts.jpa.common
+
+import jakarta.persistence.Column
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.io.Serializable
+import java.time.LocalDateTime
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+abstract class BaseEntity : Serializable {
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    var createdAt: LocalDateTime? = null
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    var updatedAt: LocalDateTime? = null
+}
